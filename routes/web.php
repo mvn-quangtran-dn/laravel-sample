def89ff21990b8988289a383bbfe0fc01f458712
Quang Tran H <quang.tran@monstar-lab.com>
2022-06-07 15:28:06 +0700

    thêm route 3

--- routes/web.php
+++ routes/web.php
@@ -100,3 +100,4 @@ Route::post('/products', 'ProductController@store')->name('products.store');
 Route::get('/products', 'ProductController@index')->name('products.index');
 //thêm route
 //thêm route 2
+//them route 3
